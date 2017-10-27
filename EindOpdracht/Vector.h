6b84e533019c5f4f1a6f0415d7dc29d821b6343a
Nkmol <sandermol95@hotmail.com>
2017-10-27 11:30:41 +0200

    Extend Vector to dynamic allocation

--- EindOpdracht/Vector.h
+++ EindOpdracht/Vector.h
@@ -1,28 +1,93 @@
 #pragma once
+#include <algorithm>
+
+using size_type = size_t;
 
 template <typename T>
 class Vector
 {
-	int used_;
-	size_t capacity_;
+	unsigned int used_;
+	unsigned int capacity_;
 	T* array_;
 public:
-	explicit Vector(const size_t& cap);
+	explicit Vector(const size_type& cap);
+	explicit Vector();
 	~Vector();
-	const size_t& capcity() const;
+	const int& capcity() const;
+	const int& used() const;
+	void push_back(const T& value);
+	void clear();
+	const T& operator[](size_type index) const;
+	void resize(const size_type& cap);
 };
 
-template <typename t>
-Vector<t>::Vector(const size_t& cap) : used_{0}, capacity_{cap}, array_{ new t[cap] }
+template <typename T>
+Vector<T>::Vector(const size_type& cap) : used_{0}, capacity_{cap}, array_{ new T[cap] }
 {
 }
 
-template <typename t>
-Vector<t>::~Vector() {
+template <typename T>
+Vector<T>::Vector() : Vector(0)
+{
+}
+
+template <typename T>
+Vector<T>::~Vector() {
 	delete[] array_;
 }
 
-template <typename t>
-const size_t& Vector<t>::capcity() const {
+template <typename T>
+const int& Vector<T>::capcity() const {
 	return capacity_;
-}
+}
+
+template <typename T>
+const int& Vector<T>::used() const {
+	return used_;
+}
+
+template <typename T>
+void Vector<T>::push_back(const T& value)
+{
+	if(used_ >= capacity_)
+	{
+		// https://crntaylor.wordpress.com/2011/07/15/optimal-memory-reallocation-and-the-golden-ratio/
+		resize(capacity_*1.5);
+	}
+
+	array_[used_++] = value;
+}
+
+template <typename T>
+void Vector<T>::clear()
+{
+	std::fill_n(array_, capacity_, T{});
+}
+
+template <typename T>
+const T& Vector<T>::operator[](size_type index) const
+{
+	return array_[index];
+}
+
+template <typename T>
+void Vector<T>::resize(const size_type& cap)
+{
+	capacity_ = cap;
+
+	if(capacity_ <= 0)
+	{
+		capacity_ = 1;
+	}
+
+	T* temp = new T[cap];
+
+	for (auto i = 0; i < used_; i++)
+	{
+		temp[i] = array_[i];
+	}
+
+	delete[] array_;
+
+	array_ = temp;
+}
